95360c374263e430dcbd2830dfa814cbb6b12bb0
Brian Watkins <brian.watkins@gmail.com>
2018-09-09 20:18:42 -0400

    Add source maps for karma tests

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      "**/*.+(tsx|ts)": "webpack",
+      "**/*.+(tsx|ts)": [ "webpack", "sourcemap" ]
     },
 
     webpack: {
@@ -45,7 +45,8 @@ module.exports = function(config) {
             ]
           }
         ]
-      }
+      },
+      devtool: "inline-source-map"
     },
 
     // test results reporter to use
