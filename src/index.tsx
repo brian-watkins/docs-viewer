28fd8af1f34cf949685c9adf6764efd59f1e78f0
Brian Watkins <brian.watkins@gmail.com>
2018-09-15 09:24:08 -0400

    Run locally with test data via webpack dev server

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,11 @@ import * as React from "react"
 import * as ReactDOM from "react-dom"
 import { BrowserRouter } from "react-router-dom"
 import { App } from "./components/App"
+import { testDocs } from "../spec/fixtures/testDocumentation"
 
 ReactDOM.render(
   <BrowserRouter>
-    <App docs={[]} />
+    <App docs={testDocs} />
   </BrowserRouter>,
   document.getElementById("example")
 )
