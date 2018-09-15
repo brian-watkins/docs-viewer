6eb35d6fe8e33d7548beb4397778b1292164db7c
Brian Watkins <brian.watkins@gmail.com>
2018-09-15 09:19:52 -0400

    Refactored documentation parser

--- src/parser/DocumentationParser.ts
+++ src/parser/DocumentationParser.ts
@@ -1,50 +1,66 @@
 import { ModuleDocumentation } from "../model/ModuleDocumentation";
 import { DocumentationBlock, AliasBlock, ValueBlock } from "../model/DocumentationBlock";
 
+
 export const parse = (doc: ModuleDocumentation): Array<DocumentationBlock> => {
-  let blocks:Array<DocumentationBlock> = []
-  const lines = doc.comment.split("\n")
+  let grouped = groupByComments(doc.comment.split("\n"))
+    .map((line) => parseLine(doc, line))
 
-  let currentComment = ""
-  for (let line of lines) {
-    if (line.startsWith("@docs")) {
-      blocks.push({ kind: "comment", value: currentComment })
-      currentComment = ""
-
-      line.substring(5).trim().split(",")
-        .map((name) => name.trim())
-        .map((name) => findBlock(doc, name))
-        .forEach((block) => blocks.push(block))
-    } else {
-      currentComment += line + "\n"
-    }
-  }
-
-  return blocks
+  return flatten(grouped)
 }
 
-const findBlock = (doc: ModuleDocumentation, name: string): DocumentationBlock => {
-  return findValueBlock(doc, name) || findAliasBlock(doc, name)
-}
 
-const findAliasBlock = (doc: ModuleDocumentation, name: string): AliasBlock => {
-  let alias = doc.aliases.find((a) => a.name === name)
+const groupByComments = (lines: Array<string>): Array<string> => (
+  lines
+    .reduce((accumulator, current) => {
+      if (current.startsWith("@docs")) {
+        return { 
+          comment: "",
+          groups: accumulator.groups.concat([ accumulator.comment, current ])
+        }
+      } else {
+        return {
+          comment: accumulator.comment + "\n" + current,
+          groups: accumulator.groups
+        }
+      }
+    }, { comment: "", groups: [] })
+    .groups
+)
 
-  if (!alias) return null
 
-  return {
-    kind: "alias",
-    ...alias
+const parseLine = (doc: ModuleDocumentation, line: string): Array<DocumentationBlock> => {
+  if (line.startsWith("@docs")) {
+    return parseDocsLine(doc, line)
+  } else {
+    return [ { kind: "comment", value: line } ]
   }
 }
 
-const findValueBlock = (doc: ModuleDocumentation, name: string): ValueBlock => {
-  let value = doc.values.find((value) => value.name == name)
+
+const parseDocsLine = (doc: ModuleDocumentation, line: string): Array<DocumentationBlock> =>
+  line.substring(5).trim().split(",")
+    .map((name) => name.trim())
+    .map((name) => findBlock(doc, name))
+
+
+const findBlock = (doc: ModuleDocumentation, name: string): DocumentationBlock => 
+  findDocBlock("value", doc.values, name) || 
+  findDocBlock("alias", doc.aliases, name)
+
+
+const findDocBlock = (kind: string, docList: Array<{name: string}>, name: string): DocumentationBlock => {
+  let value = docList.find((value) => value.name == name)
   
   if (!value) return null
    
   return { 
-    kind: "value",
+    kind: kind,
     ...value
-  } as ValueBlock
+  } as DocumentationBlock
+}
+
+
+const flatten = <T>(list:Array<Array<T>>): Array<T> => {
+  return [].concat(...list)
 }
