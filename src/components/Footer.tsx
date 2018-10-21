447a532241d10658c88b8368c76e6c2468b82955
Brian Watkins <brian.watkins@gmail.com>
2018-10-21 11:53:10 -0400

    Add link to repo in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,9 @@ import * as React from "react"
 
 export const Footer = () => (
   <div id="footer">
-    Source available at http://github.com/brian-watkins
+    Source available at:&nbsp; 
+    <a href="https://github.com/brian-watkins/docs-viewer" target="_blank">
+      https://github.com/brian-watkins/docs-viewer
+    </a>
   </div>
 )
