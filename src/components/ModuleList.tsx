0def4d489af8f0f9aa9a6da86712706ff0037f5b
Brian Watkins <brian.watkins@gmail.com>
2019-05-05 15:27:25 -0400

    ModuleList component handles layout
    - css layout was too unwieldy so had to do it in code

--- src/components/ModuleList.tsx
+++ src/components/ModuleList.tsx
@@ -12,24 +12,61 @@ export interface ModuleListProps {
 }
 
 export class ModuleList extends React.Component<ModuleListProps, {}> {
-  render = () => (
-      <div id="module-list">
-        <div className="stick">
-          <h1>Modules</h1>
-          { this.showModules() }
+  render = () => {
+    const [ group1, group2 ] = this.splitGroups(this.props.docs)
+    
+    return <div id="module-list">
+      <div className="stick">
+        <h1>Modules</h1>
+        <div data-module-list>
+          <div>
+            { this.showModules(this.props.docs, group1) }
+          </div>
+          <div>
+            { this.showModules(this.props.docs, group2) }
+          </div>
         </div>
       </div>
-  )
+    </div>
+  }
 
-  showModules = () => (
+  splitGroups = (modules: Array<ModuleDocumentation>): [Array<string>, Array<string>] => {
+    const groups = Array.from(this.findGroups(modules).values())
+    const group1 = groups.slice(0, Math.ceil(groups.length / 2))
+    const group2 = groups.slice(Math.ceil(groups.length / 2))
+    return [group1, group2]
+  }
+
+  showModules = (docs: Array<ModuleDocumentation>, modulePrefixes: Array<string>) => (
     <PackageVersionContext.Consumer>
       {packageVersion => (
-        <ul>
-          { this.props.docs.map(this.showModuleItem(packageVersion)) }
-        </ul>
+        modulePrefixes.map(this.showModuleGroup(docs, packageVersion, modulePrefixes[0]))
       )}
     </PackageVersionContext.Consumer>
   )
+
+  findGroups = (modules: Array<ModuleDocumentation>): Set<string> => {
+    if (modules.length === 0) return new Set()
+
+    const rootLength = modules[0].name.split(".").length + 1
+
+    return modules.reduce((accumulated, current) => {
+      return accumulated.add(current.name.split(".").slice(0, rootLength).join("."))
+    }, new Set())
+  }
+
+  showModuleGroup = (docs: Array<ModuleDocumentation>, packageVersion: PackageVersion, root: string) => (prefix: string) => {
+    let items = []
+    if (prefix === root) {
+      items = docs.filter((m) => m.name === prefix).map(this.showModuleItem(packageVersion))
+    } else {
+      items = docs.filter((m) => m.name.startsWith(prefix)).map(this.showModuleItem(packageVersion))
+    }
+
+    return <ul key={prefix}>
+      { items }
+    </ul>
+  }
 
   showModuleItem = (packageVersion: PackageVersion) => (moduleDoc: ModuleDocumentation) => (
     <ListItemLink
